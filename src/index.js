fa9d815e922aff917f65632a919aa34939ba79ef
Daiane Oltramari <daianeoltramari@gmail.com>
2022-01-19 16:53:07 -0300

    Style.css (Texto do h1 centralizado), Validator.js e index.js (Voltou para a documento original)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
 import validator from './validator.js';
 
-document.querySelector(".submitBtn").addEventListener("click", document);
-
 console.log(validator);
 
-let userNumInput = document.getElementById("card");
-
-function getUserInput(){
-   return userNumInput.value;  
-}
-
-document.getElementById("submitBtn").addEventListener("click", function(){
-   document.getElementById("resultDiv").innerHTML = luhnCheck();
-}, false);

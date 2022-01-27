7056f7592a4a0799b4e20af887289a70f461a8c1
Daiane Oltramari <daianeoltramari@gmail.com>
2022-01-27 13:56:51 -0300

    alteração no README

--- README.md
+++ README.md
@@ -9,10 +9,16 @@
 
 ## 1. Introdução
 
-Esté projeto é feito com as Tecnologias : HTML, CSS e JavaScript (É uma aplicação web). Foi usado o
-[algoritmo de Luhn](https://en.wikipedia.org/wiki/Luhn_algorithm), que é um método de soma de verificação, 
+Neste projeto é usado o [algoritmo de Luhn](https://en.wikipedia.org/wiki/Luhn_algorithm), que é um método de soma de verificação, 
 usado para validar números de identificação, como o IMEI de telefones celulares, cartões de crédito etc.
 
+## O dígito de verificação é computado da seguinte forma:
+
+Se o número já contiver o dígito de verificação, solte esse dígito para formar a "carga". O dígito de verificação é na maioria das vezes o último dígito.
+Com a carga, comece a partir do dígito mais certo. Movendo-se para a esquerda, o dobro do valor de cada segundo dígito (incluindo o dígito mais à direita).
+Soma os dígitos do valor resultante em cada posição.
+Soma os valores resultantes de todas as posições, {\displaystyle s}s.
+O dígito de verificação é calculado por {\displaystyle ((10-s\operatorname {mod} 10)\nome do operador {mod} 10)}{\displaystyle ((10-s\operatorname {mod} 10)\nome do operador {mod} 10)}
 
 ---
 
--- src/validator.js
+++ src/validator.js
@@ -2,39 +2,25 @@ const validator = {
 
    isValid: function (cardNumber) {
      console.log("value card "+ cardNumber);
-     if (cardNumber == "" || cardNumber == null || cardNumber.length > 16) {
+     if (cardNumber == "" || cardNumber == null || cardNumber.length < 16) { // estou dando condições: caso o unput estiver vazio, null ou
        alert("Informação está incorreta");
      }
  
      let cardValidate = "";
      let cardArr = Array.from(cardNumber).reverse;
  
-     for (let i = 0; (i = cardArr.length); i++){
-       if (i % 2 != 0) {
-         cardArr[i] = cardArr[i] * 2;
+     for (let i = cardArr.length -1; i >= 0; i--){
+       if (i % 2 !== 0) {
+        singleNums.push(cardArr[i]);
+      }else{
+         doubleNums.push((cardArr[i] * 2).toString());
        }
+    }},
  
-     if (cardArr[i] > 9) {
-       let rest = cardArr[i] % 10;
-       let aprox = Math.trunc(cardArr[i] / 10);
-       let sum = rest + aprox;
-       cardArr[i] = sum;
-     }}
- 
-     let total = cardArr.reduce(
-       (total, currentElement) => total + currentElement
-     );
- 
-     if (total % 10 == 0) {
-       cardValidate = true;
-     } else {
-       cardValidate = false;
-     }
- 
-     return true;
+
+
+
     
-   },
- 
    maskify: function (cardNumber) {
      let digitMask = Array.from(cardNumber);
  

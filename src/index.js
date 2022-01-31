33210e1fd7842733d2734a601a5af30c78228fc1
Daiane Oltramari <daianeoltramari@gmail.com>
2022-01-31 16:43:56 -0300

    alterações

--- src/index.js
+++ src/index.js
@@ -2,31 +2,32 @@ import validator from "./validator.js";
 
 const button = document.getElementById("button");
 const input = document.getElementById("cardNumber");
+const alert = document.getElementById("alert");
 const _validator = validator;
-
 class Main {
   constructor() {
     button.onclick = function () {
+      alert.style.visibility = 'visible'; // deixar visivel 
       this.validate();
     }.bind(this);
-
     input.onkeyup = function () {
       this.masker();
     }.bind(this);
   }
-
   validate() {
     let card = input.value;
     const validator = _validator.isValid(card);
     if (validator == true) {
     } else validator == false;
   }
-
   masker() {
     let cardNumber = input.value;
     const mask = _validator.maskify(cardNumber);
     input.value = mask;
   }
+  alert(){
+    alert.style.visibility = true;
+    
+  }
 }
-
 new Main();
--- src/validator.js
+++ src/validator.js
@@ -1,42 +1,36 @@
 const validator = {
   isValid: function (cardNumber) {
-    let cardValidate = ""; // Variavel que guardará o resultado do cartão (True ou False).
+    // função que recebe como parâmetro o valor do meu input
     let cardArr = Array.from(cardNumber).reverse; // Pegou o valor do input e transformou em uma Array que será percorrida ao inverso
-    let soma = 0 // variavel que guarda o valor da soma
-
-    if (cardNumber =="" || cardNumber.length < 16){
+    let soma = 0; // variavel que guarda o valor da soma
+    if (cardNumber == "" || cardNumber.length < 16) {
       alert("Informação está incorreta");
     }
-
-    for (let i = 0; (i = cardArr.length); i++) { //loop que percorre o array invertida
-      if (i % 2 != 0) { // Se o numero for impar
+    for (let i = 0; (i = cardArr.length); i++) {
+      //loop que percorre o array invertida
+      if (i % 2 != 0) {
+        // Se o numero for impar
         cardArr[i] = cardArr[i] * 2; // Multiplica por 2
-    }
-
-      if (cardArr[i] > 9) { //Se o numero for maior que 9
-        cardArr[i] -= 9 //subtrai por 9
-        soma += cardArr[i] // soma os numeros 
       }
-  
-    if (soma % 10 == 0) { //Se o numero for divisivel por 10
-      cardValidate = true; // vardadeiro
-    } else {
-      cardValidate = false; // Falso
+      if (cardArr[i] > 9) {
+        //Se o numero for maior que 9
+        cardArr[i] -= 9; //subtrai por 9
+        soma += cardArr[i]; // soma os numeros
+      }
     }
-
-    return true;
-}},
-
+    if (soma % 10 == 0) {
+      //Se o numero for divisivel por 10, então é verdadeiro
+      return true;
+    }
+    return false;
+  },
   maskify: function (cardNumber) {
     let digitMask = Array.from(cardNumber);
-
     for (let i = 0; i < digitMask.length - 4; i++) {
       digitMask[i] = "#";
     }
     let join = digitMask.join("");
-
     return join;
   },
 };
-
 export default validator;

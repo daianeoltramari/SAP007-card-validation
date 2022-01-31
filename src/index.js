f221f2f7a3287b6c3e7ff6cba70ed68dbb21a024
Daiane Oltramari <daianeoltramari@gmail.com>
2022-01-31 11:10:31 -0300

    função IsValid

--- src/index.js
+++ src/index.js
@@ -5,30 +5,28 @@ const input = document.getElementById("cardNumber");
 const _validator = validator;
 
 class Main {
+  constructor() {
+    button.onclick = function () {
+      this.validate();
+    }.bind(this);
 
-      constructor() {
-        button.onclick = function() {
-          this.validate();
-        }.bind(this);
+    input.onkeyup = function () {
+      this.masker();
+    }.bind(this);
+  }
 
-        input.onkeyup = function(){
-          this.masker();
-        }.bind(this);
-      }
+  validate() {
+    let card = input.value;
+    const validator = _validator.isValid(card);
+    if (validator == true) {
+    } else validator == false;
+  }
 
-      validate() {
-        let card = input.value;
-        const validator = _validator.isValid(card);
-        if (validator == true) {
-        } else validator == false;
-      }
-      
-      masker() {
-        let cardNumber = input.value;
-        const mask = _validator.maskify(cardNumber);
-        input.value = mask;
-      }
-    
-    }
-    
+  masker() {
+    let cardNumber = input.value;
+    const mask = _validator.maskify(cardNumber);
+    input.value = mask;
+  }
+}
+
 new Main();
--- src/validator.js
+++ src/validator.js
@@ -1,51 +1,44 @@
 const validator = {
+  isValid: function (cardNumber) {
+    let cardValidate = "";
+    let cardArr = Array.from(cardNumber).reverse;
 
-   isValid: function (cardNumber) {
-     // estou dando condições: caso o input estiver vazio, null ou for menor que 16 irá mostrar a msg
-     if (cardNumber == "" || cardNumber == null || cardNumber.length < 16) { 
-       alert("Informação está incorreta");
-     }
- 
-     let cardValidate = "";
-     let cardArr = Array.from(cardNumber).reverse;
- 
-     for (let i = cardArr.length -1; i >= 0; i--){
-       if (i % 2 !== 0) {
-        singleNums.push(cardArr[i]);
-      }else{
-         doubleNums.push((cardArr[i] * 2).toString());
-       }
+    for (let i = 0; (i = cardArr.length); i++) {
+      if (i % 2 != 0) {
+        cardArr[i] = cardArr[i] * 2;
+      }
 
-  //juntando faz um array para uma string e eu os divido novamente
-  //para que cada número seja um único dígito e converta de volta para array
+      if (cardArr[i] > 9) {
+        let rest = cardArr[i] % 10;
+        let aprox = Math.trunc(cardArr[i] / 10);
+        let sum = rest + aprox;
+        cardArr[i] = sum;
+      }
+    }
 
+    let total = cardArr.reduce(
+      (total, currentElement) => total + currentElement
+    );
 
-       doubleNums = doubleNums.join("").split("");  
-       finalArry = doubleNums.concat(singleNums);
-       
-       for(var j = 0; j<finalArry.length; j++){
-         sum += parseInt(finalArry[j]);
-       }
-       
-       if(sum % 10 === 0){
-         validCard = true;
-       }
-    }},
- 
+    if (total % 10 == 0) {
+      cardValidate = true;
+    } else {
+      cardValidate = false;
+    }
 
-  
+    return true;
+  },
 
-    
-   maskify: function (cardNumber) {
-     let digitMask = Array.from(cardNumber);
- 
-     for (let i = 0; i < digitMask.length - 4; i++) {
-       digitMask[i] = "#";
-     }
-     let join = digitMask.join("");
- 
-     return join;
-   },
- };
- 
- export default validator;
+  maskify: function (cardNumber) {
+    let digitMask = Array.from(cardNumber);
+
+    for (let i = 0; i < digitMask.length - 4; i++) {
+      digitMask[i] = "#";
+    }
+    let join = digitMask.join("");
+
+    return join;
+  },
+};
+
+export default validator;

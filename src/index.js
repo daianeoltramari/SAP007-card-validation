b98400d86f7ffb9c545e15931f50069da5ff1324
Daiane Oltramari <daianeoltramari@gmail.com>
2022-02-01 13:31:03 -0300

    pasta test

--- src/index.js
+++ src/index.js
@@ -4,30 +4,66 @@ const button = document.getElementById("button");
 const input = document.getElementById("cardNumber");
 const alert = document.getElementById("alert");
 const _validator = validator;
+
 class Main {
+  
   constructor() {
+    this.cardNumberAux = [];
+
     button.onclick = function () {
-      alert.style.visibility = 'visible'; // deixar visivel 
       this.validate();
     }.bind(this);
-    input.onkeyup = function () {
-      this.masker();
+    
+    input.onkeydown = function (event) {
+      if(event.keyCode === "Backspace"){
+        this.cardNumberAux.pop();
+        console.log(this.cardNumberAux);
+        return;
+      }
+
+      this.masker(event);
     }.bind(this);
   }
+
   validate() {
-    let card = input.value;
-    const validator = _validator.isValid(card);
-    if (validator == true) {
-    } else validator == false;
-  }
-  masker() {
-    let cardNumber = input.value;
-    const mask = _validator.maskify(cardNumber);
-    input.value = mask;
-  }
-  alert(){
-    alert.style.visibility = true;
+    //let card = input.value;
+    console.log("Card Number not masked", this.cardNumberAux)
+    let result = _validator.isValid(this.cardNumberAux);
     
+    if(result){
+      this.alert("Cartão valido");
+    }else{
+      this.alert("Cartão invalido");
+    }
   }
+
+  masker(event) {
+    console.log(event);
+    if(this.cardNumberAux.length == 16 && event.keyCode != 32){
+      return;
+    }
+    console.log(this.cardNumberAux);
+   
+    this.cardNumberAux.push(event.key);    
+    let mask = _validator.maskify(input.value);
+    input.value = mask;
+
+  }
+
+  alert(text){
+    alert.style.visibility = 'visible'; // deixar visivel 
+    alert.innerHTML = text;
+  }
+
+  isNumber(value) {
+    var regex = /[^0-9.]/;
+    regex.lastIndex = 0;
+    if (regex.test(value)) {
+      return false;
+    }
+
+    return true;
+  }
+
 }
 new Main();
--- src/validator.js
+++ src/validator.js
@@ -1,21 +1,22 @@
 const validator = {
   isValid: function (cardNumber) {// função que recebe como parâmetro o valor do meu input
-    let cardArr = Array.from(cardNumber).reverse; // Pegou o valor do input e transformou em uma Array que será percorrida ao inverso
+    if (cardNumber == "" || cardNumber.length < 16) { // Condições: Se o valor do input for vazio ou menor que 16 caracteres, mostrar msg
+      console.log("Quantidade de caracteres invalido")
+      return false;
+    }
+
+    //let cardArr = Array.from(cardNumber).reverse; // Pegou o valor do input e transformou em uma Array que será percorrida ao inverso
     let soma = 0; // variavel que guarda o valor da soma
 
-    
-    if (cardNumber == "" || cardNumber.length < 16) { // Condições: Se o valor do input for vazio ou menor que 16 caracteres, mostrar msg
-      alert("Informação está incorreta");
-    }
-    for (let i = 0; (i = cardArr.length); i++) { //loop que percorre o array invertida
-      if (i % 2 != 0) {// Se o numero for impar
-        cardArr[i] = cardArr[i] * 2; // Multiplica por 2
-      }
-      if (cardArr[i] > 9) {//Se o numero for maior que 9
-        cardArr[i] -= 9; //subtrai por 9
-        soma += cardArr[i]; // soma os numeros
-      }
-    }
+    // for (let i = 0; (i = cardNumber.length); i++) { //loop que percorre o array invertida
+    //   if (i % 2 != 0) {// Se o numero for impar
+    //     cardNumber[i] = cardNumber[i] * 2; // Multiplica por 2
+    //   }
+    //   if (cardNumber[i] > 9) {//Se o numero for maior que 9
+    //     cardNumber[i] -= 9; //subtrai por 9
+    //     soma += cardNumber[i]; // soma os numeros
+    //   }
+    // }
     if (soma % 10 == 0) {//Se o numero for divisivel por 10
       return true; //então é verdadeiro
     }
